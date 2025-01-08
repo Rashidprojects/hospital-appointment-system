5e00177848af536bc54d2f185056cf1fe407b998
Rashidprojects <rashidgitproject@gmail.com>
2025-01-08 23:06:02 +0530

    Tailwind css and Shadcn Ui successfully configured

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <div>
      <h1>Welcome to Appointment Dashboard!!!</h1>
+     <Button className='bg-red-500'>Hello Click !</Button>
     </div>
   )
 }
