67dc8e59a0af769299dcb562452d9c7c04910a60
Rashidprojects <rashidgitproject@gmail.com>
2025-01-15 23:48:14 +0530

    shadcn default colors changed to WellnessDesk theme

--- client/src/layouts/AdminLayout.tsx
+++ client/src/layouts/AdminLayout.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import type { LayoutProps } from '../types/layout'
-
+import { Header } from '../components/admin'
 
 
 const AdminLayout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
-      <h1>Admin Layout</h1>
-      <nav>this navbar</nav>
-      <main>{children}</main>
+      <Header />
+      <main className='min-h-screen container mx-auto'>{children}</main>
       <footer>This is footer</footer>
     </div>
   )
